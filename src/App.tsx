90955d47650f8515f20071d58d2bd5ef01ea82cf
akhil-haridas <4khilharidas@gmail.com>
2025-08-03 13:32:05 +0530

    app route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppRouter } from "@routes";
 
-function App() {
-  return <AppRouter />;
-}
+const queryClient = new QueryClient();
 
-export default App;
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
