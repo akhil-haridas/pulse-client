960c84c3d03b3fb911107c9f2056e02fc7aa740f
akhil-haridas <4khilharidas@gmail.com>
2025-08-03 13:34:33 +0530

    browser route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { AppRouter } from "@routes";
 
-const queryClient = new QueryClient();
+function App() {
+  const queryClient = new QueryClient();
+  return (
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </React.StrictMode>
+  )
+}
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+export default App;
